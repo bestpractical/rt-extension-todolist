cf82fc495d603d05a1dbc2cf3abfd998c15d95c4
Craig Kaiser <craig@bestpractical.com>
2019-04-24 12:01:01 -0400

    Use ES5 JS syntax

--- static/js/rt-extension-todolist.js
+++ static/js/rt-extension-todolist.js
@@ -1,5 +1,5 @@
-const UpdateTodoList = (Object) => {
-    const CustomField = jQuery('#RT-TodoList-Select').val()
+function UpdateTodoList (Object) {
+    const CustomField = jQuery('#RT-TodoList-Select').val();
     fetch(RT.Config.WebHomePath + "/Helpers/TodoList?UpdateTodoList=1&ObjectId="+Object+"&CustomField="+CustomField, {
     }).then(response => (response.json()))
     .then(json => {
@@ -7,19 +7,19 @@ const UpdateTodoList = (Object) => {
       })
       .catch(function (error) {
         console.log('Request failed', error)
-      })
-}
+      });
+};
 
-const UpdateTodos = () => {
-    let values = {}
+function UpdateTodos () {
+    let values = {};
     jQuery('#RT-TodoList :checkbox').each(function(){
         if ( jQuery(this).is(":checked") ) {
             values[this.id] = document.querySelectorAll("[for='"+this.id+"']")[0].innerHTML;
         } else {
             values[this.id] = 'RT-TodoList-Remove-'+document.querySelectorAll("[for='"+this.id+"']")[0].innerHTML;
         }
-    })
-    values['UpdateTodo'] = 1
+    });
+    values['UpdateTodo'] = 1;
     fetch(RT.Config.WebHomePath + "/Helpers/TodoList", {
         method: 'POST',
         headers: {
@@ -27,5 +27,5 @@ const UpdateTodos = () => {
         'Content-Type': 'application/json'
         },
         body: JSON.stringify(values)
-    })
-}
+    });
+};
